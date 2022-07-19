07983e473b8a47816870aca5c1b64a60004a8215
JPEGimage <107156887+JPEGimage@users.noreply.github.com>
2022-07-19 18:46:31 -0300

    Inventário e UI
    Iniciando criação do inventário e sistema de itens, além de aprimorar as funções de motões da UI

--- Biomaker/Assets/Scripts/UI/MenuController.cs
+++ Biomaker/Assets/Scripts/UI/MenuController.cs
@@ -5,16 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public string SceneName;
-
-    public void ChangeS()
+    public void ChangeS(string SceneName)
     {
         SceneManager.LoadScene(SceneName);
+    }
+
+    public void VizualizeTrue(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+    public void VizualizeFalse(GameObject obj)
+    {
+        obj.SetActive(false);
     }
 
     public void Quit()
     {
         Debug.Log("Jogo fechado.");
         Application.Quit();
+    }
+    public void Oi(string o)
+    {
+        Debug.Log(o);
     }
 }
